1a351d751b02519c0c720604ef9a1e926d90b3be
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-02-20 16:42:44 +0100

    add some endpoints to api

--- app.js
+++ app.js
@@ -1,20 +1,66 @@
-import express from 'express'
-import db from './db/db'
+import express from "express";
+import bodyParser from "body-parser";
+import db from "./db/db";
 
-const app = express()
+const app = express();
 
-app.get('/api/v1/todos', (err, req, res, next) => {
-  console.log(err)
-  res.status(200).send({
-    success: 'true',
-    message: 'todos retrievessd',
-    todos: db
-  })
-  console.log('oops')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 5000
+app.get("/api/v1/todos", (req, res, err) => {
+  console.log(err);
+  return res.status(200).send({
+    success: "true",
+    message: "todos retrievessd",
+    todos: db,
+  });
+});
+
+app.post("/api/v1/todos", (req, res) => {
+  if (!req.body.title) {
+    return res.status(400).send({
+      succes: "false",
+      message: "Title is required",
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "Descriptions is required",
+    });
+  }
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  db.push(todo);
+  return res.status(201).send({
+    sucess: "true",
+    message: "todo was added successfully",
+    todo,
+  });
+});
+
+app.get("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  db.map(todo => {
+    if (todo.id === id) {
+      return res.status(200).send({
+        success: "true",
+        message: "todo retirev successfully",
+        todo,
+      });
+    }
+  });
+  return res.status(404).send({
+    sucess: "false",
+    message: "todo doesnt exist",
+    id,
+  });
+});
+
+const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
