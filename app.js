ef4491ef08014a17687f89a69de3996ef998e097
Eleonor Dammfors <eleonor.dammfors@gmail.com>
2020-02-22 15:24:25 +0100

    add put and delete endpoints

--- app.js
+++ app.js
@@ -59,6 +59,72 @@ app.get("/api/v1/todos/:id", (req, res) => {
   });
 });
 
+app.delete("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+
+      return res.status(200).send({
+        succes: "true",
+        message: "deleted this todo",
+        todo,
+      });
+    }
+  });
+
+  return res.status(404).send({
+    succes: "false",
+    message: "wasnt able to delete",
+  });
+});
+
+app.put("/api/v1/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(400).send({
+      success: "false",
+      message: "couldnt find this todo",
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: "false",
+      message: "needs title",
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: "false",
+      message: "needs description dude",
+    });
+  }
+
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  db.splice(itemIndex, 1, updatedTodo);
+  return res.status(200).send({
+    success: "true",
+    message: "cool it's updated now",
+    updatedTodo,
+  });
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
--- db/db.js
+++ db/db.js
@@ -1,9 +1,14 @@
 const todos = [
-    {
-        id: 1,
-        title: "Buy groceries",
-        description: "Buy groceries for todays dinner"
-    }
+  {
+    id: 1,
+    title: "Buy groceries",
+    description: "Buy groceries for todays dinner",
+  },
+  {
+    id: 2,
+    title: "Buy wine",
+    description: "white wine and cava please",
+  },
 ];
 
 export default todos;
